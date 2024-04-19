5d81265f79d5358cd50a446f770ed168e3e46c10
Stas254 <Stas254@yandex.ru>
2024-04-19 21:59:07 +0300

    1. Cоздал ветку с названием Lesson_11_gradle; 2. В ветке Lesson_11_gradle создал проект, используя Gradle; 3. Добавил зависимости на такие инструменты как JUnit 5, Selenium; 4. C&P.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.seleniumhq.selenium:selenium-java:4.19.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
 }
 
 tasks.test {
